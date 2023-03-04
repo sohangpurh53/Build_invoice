4e05ffe20548977a4cd43b304ae7ffb33db5128e
sohangpurh53 <113816299+sohangpurh53@users.noreply.github.com>
2023-03-04 21:06:14 +0530

    Update settings.py

--- invoicebuilder/settings.py
+++ invoicebuilder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://invoice-builder.onrender.com']
+ALLOWED_HOSTS = ['invoice-builder.onrender.com']
 
 
 # Application definition
